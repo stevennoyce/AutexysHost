7266191ed2b618ced44aa5456d5abbd0f1ecb0c1
stevennoyce <steven.noyce@gmail.com>
2018-10-03 09:08:16 -0400

    Minor improvements to AFM plots

--- source/utilities/DataPlotterUtility.py
+++ source/utilities/DataPlotterUtility.py
@@ -756,13 +756,15 @@ def plotAFMdeviationsVsX(deviceHistory, identifiers, mode_parameters=None):
 	
 	# Build Color Map
 	colors = colorsFromMap(plot_parameters['AFMdeviationsVsX']['colorMap'], 0, 0.87, len(deviceHistory))['colors']
-	# E07_N is a sister device, E22N_10000 is another with 40nm device, 5 fin devices E33N E64N, Bigger cavity E27N and E27P 5 fin
+	# E07N is a sister device, E22N_10000 is another with 40nm device, 5 fin devices E33N E64N, Bigger cavity E27N and E27P 5 fin
+	# Bonded devices E07N_10000 (pins 7-8), E08N_10000 (pins 9-10), E22N_10000 (pins 11-12), E27N_10000 (pins 13-14), E33N_10000 (pins 15-16)
+	# CNT devices that could be used for AFM: C127V2-3 C127X15-16
 	# Plot
 	for i in range(len(deviceHistory)):
 		current = np.array(deviceHistory[i]['Results']['id_data'])
 		currentLinearFit = np.polyval(np.polyfit(range(len(current)), current, 1), range(len(current)))
 		currentLinearized = current - currentLinearFit
-		currentLinearized = currentLinearized - max(currentLinearized)
+		currentLinearized = currentLinearized - np.median(currentLinearized)
 		
 		Vxs = np.array(deviceHistory[i]['Results']['smu2_v2_data'])
 		Xs = -Vxs/0.157
@@ -800,7 +802,9 @@ def plotAFMdeviationsVsXY(deviceHistory, identifiers, mode_parameters=None):
 		current = np.array(deviceHistory[i]['Results']['id_data'])
 		currentLinearFit = np.polyval(np.polyfit(range(len(current)), current, 1), range(len(current)))
 		currentLinearized = current - currentLinearFit
-		currentLinearized = currentLinearized - max(currentLinearized)
+		currentLinearized = currentLinearized - np.median(currentLinearized)
+		
+		# currentLinearized[np.where(np.array(deviceHistory[i]['Results']['smu2_v1_data']) - max(deviceHistory[i]['Results']['smu2_v1_data']) < -2.2*0.157)[0]] = 0
 		
 		Vxs.extend(deviceHistory[i]['Results']['smu2_v2_data'])
 		Vys.extend(deviceHistory[i]['Results']['smu2_v1_data'])
@@ -812,6 +816,8 @@ def plotAFMdeviationsVsXY(deviceHistory, identifiers, mode_parameters=None):
 	Xs = Xs - np.min(Xs)
 	Ys = Ys - np.min(Ys)
 	
+	# currents[np.where(Xs < 0.5)[0]] = 0
+	
 	c, a, b = zip(*sorted(zip(np.array(currents)*1e9, Xs, Ys), reverse=True))
 	line = ax.scatter(a, b, c=c, cmap=plot_parameters['AFMdeviationsVsXY']['colorMap'], alpha=0.6)
 	# line = ax.scatter(Xs, Ys, c=np.array(currents)*1e9, cmap=plot_parameters['AFMdeviationsVsXY']['colorMap'], alpha=0.6)
@@ -822,8 +828,8 @@ def plotAFMdeviationsVsXY(deviceHistory, identifiers, mode_parameters=None):
 	# if(len(deviceHistory) == len(mode_parameters['legendLabels'])):
 		# setLabel(line, mode_parameters['legendLabels'][i])
 	
-	ax.set_ylabel('Y Voltage [V]')
-	ax.set_xlabel('X Voltage [V]')
+	ax.set_ylabel('Y Position [$\mu$m]')
+	ax.set_xlabel('X Position [$\mu$m]')
 	
 	# Add Legend and save figure
 	# addLegend(ax, loc=mode_parameters['legendLoc'], title=getLegendTitle(deviceHistory, identifiers, 'SubthresholdCurve', 'runConfigs', 'GateSweep', mode_parameters, includeVdsSweep=True, includeSubthresholdSwing=False))
