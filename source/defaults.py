252eccff530d5ba08640d68809b1a9ad048a6de3
Steven Noyce <steven.noyce@gmail.com>
2018-10-05 16:10:24 -0400

    Added combined default parameters and descriptions

--- source/defaults.py
+++ source/defaults.py
@@ -118,6 +118,140 @@ default_parameters = {
 }
 
 
+default_parameters_combined = {
+	'runConfigs': {
+		'GateSweep':{
+			'dependencies':[],
+			'saveFileName': {'type':'constant', 'default': 'GateSweep'},
+			'isFastSweep': {'type':'bool', 'default': 'False'},
+			'isAlternatingSweep': {'type':'bool', 'default': 'False'},
+			'pulsedMeasurementOnTime': {'type':'float', 'units':'s', 'default': 0},
+			'pulsedMeasurementOffTime': {'type':'float', 'units':'s', 'default': 0},
+			'stepsInVGSPerDirection': {'type':'int', 'units':'#', 'default': 100},
+			'pointsPerVGS': {'type':'int', 'units':'#', 'default': 1},
+			'complianceCurrent':	{'type':'float', 'units':'A', 'default': 100e-6},
+			'drainVoltageSetPoint':	{'type':'float', 'units':'V', 'default': -0.5},
+			'gateVoltageMinimum': 	{'type':'float', 'units':'V', 'default': -15},
+			'gateVoltageMaximum': 	{'type':'float', 'units':'V', 'default': 15}
+		},
+		'DrainSweep':{
+			'saveFileName': 'DrainSweep',
+			'isFastSweep': {'type':'bool', 'default':False},
+			'stepsInVDSPerDirection': {'type':'int', 'units':'#', 'default':100},
+			'pointsPerVDS': {'type':'int', 'units':'#', 'default':1},
+			'complianceCurrent':	{'type':'float', 'units':'A', 'default':100e-6},
+			'gateVoltageSetPoint':	{'type':'float', 'units':'V', 'default':0},
+			'drainVoltageMinimum': 	{'type':'float', 'units':'V', 'default':0},
+			'drainVoltageMaximum': 	{'type':'float', 'units':'V', 'default':0.5}
+		},
+		'BurnOut':{
+			'dependencies':[],
+			'saveFileName': {'type:': 'constant', 'default': 'BurnOut'},
+			'pointsPerRamp': {'type:': 'int', 'units':'#', 'default':50},
+			'pointsPerHold': {'type':'int', 'units':'#', 'default': 50},
+			'complianceCurrent':	{'type':'float', 'units':'V', 'default': 2e-3},
+			'thresholdProportion':	{'type':'float', 'units':'', 'default': 0.92},
+			'minimumAppliedDrainVoltage': {'type':'float', 'units':'V', 'default': 1.1},
+			'gateVoltageSetPoint':	{'type':'float', 'units':'V', 'default': 15.0},
+			'drainVoltageMaxPoint':	{'type':'float', 'units':'V', 'default': 5},
+			'drainVoltagePlateaus': {'type':'int', 'units':'#', 'default': 5}
+		},
+		'StaticBias':{
+			'dependencies':[],
+			'saveFileName': {'type':'constant', 'default':'StaticBias'},
+			'totalBiasTime': {'type':'float', 'units':'s', 'default': 60},
+			'measurementTime': {'type':'float', 'units':'s', 'default': 10},
+			'complianceCurrent': {'type':'float', 'units':'A', 'default': 100e-6},
+			'delayBeforeMeasurementsBegin': {'type':'float', 'units':'s', 'default': 0},
+			'gateVoltageSetPoint': 	{'type':'float', 'units':'V', 'default': -15},
+			'drainVoltageSetPoint':	{'type':'float', 'units':'V', 'default': -0.5},
+			'gateVoltageWhenDone':  {'type':'float', 'units':'V', 'default': 0},
+			'drainVoltageWhenDone': {'type':'float', 'units':'V', 'default': 0},
+			'floatChannelsWhenDone': {'type':'bool', 'default': False},
+			'delayWhenDone': {'default': 0}
+		},
+		'AutoBurnOut':{
+			'dependencies':['BurnOut'],
+			'targetOnOffRatio': {'type':'float', 'units':'', 'default': 80},
+			'limitBurnOutsAllowed': {'type':'int', 'units':'#', 'default': 8},
+			'limitOnOffRatioDegradation': {'type':'float', 'units':'V', 'default': 0.7}
+		},
+		'AutoGateSweep':{
+			'dependencies':['GateSweep'],
+			'sweepsPerVDS': {'type':'int', 'units':'#', 'default': 1},
+			'drainVoltageSetPoints': {'default': [-0.100, -0.010]},
+			'applyStaticBiasBetweenSweeps': {'type':'bool'},
+			'delayBetweenSweeps': {'type':'float', 'units':'s', 'default': 0},
+			'timedSweepStarts': {'type':'bool', 'default': False}
+		},
+		'AutoDrainSweep':{
+			'gateVoltageSetPoint':	{'type':'float', 'units':'V', 'default':[-10.0, -5.0, 0]},
+			'sweepsPerVGS': {'type':'int', 'units':'#', 'default': 1},
+			'delayBetweenSweeps': {'type':'float', 'units':'s', 'default': 0}
+		},
+		'AutoStaticBias':{
+			'dependencies':['StaticBias','GateSweep'],
+			'numberOfStaticBiases': {'type':'int', 'units':'#', 'default': 1},
+			'doInitialGateSweep': {'type':'bool', 'default': True},
+			'applyGateSweepBetweenBiases': {'type':'bool', 'default': False},
+			'turnChannelsOffBetweenBiases': {'type':'bool'},
+			'channelsOffTime': {'type':'float', 'units':'V'},
+			'firstDelayBeforeMeasurementsBegin': {'type':'float', 'units':'s', 'default': 0},
+			'numberOfBiasesBetweenIncrements': {'type':'int', 'units':'#', 'default': 1},
+			'incrementStaticGateVoltage': {'type':'float', 'units':'V', 'default': 0},
+			'incrementStaticDrainVoltage': {'type':'float', 'units':'V', 'default': 0},
+			'incrementGateVoltageWhenDone': {'type':'float', 'units':'V', 'default': 0},
+			'incrementDrainVoltageWhenDone': {'type':'float', 'units':'V', 'default': 0},
+			'incrementDelayBeforeReapplyingVoltage': {'type':'float', 'units':'s', 'default': 0},
+			'shuffleDelaysBeforeReapplyingVoltage': {'type':'bool', 'default': False}
+		},
+		'AFMControl':{
+			'saveFileName': {'type':'constant', 'default':'AFMControl'},
+			'lines': {'type':'int', 'units':'#', 'default':3},
+			'scanRate': {'type':'int', 'units':'#', 'default':1},
+			'napOn': {'type':'bool', 'default':True},
+			'drainVoltageSetPoint': {'type':'float', 'units':'V', 'default': 0.01},
+			'gateVoltageSetPoint': {'type':'float', 'units':'V', 'default': 0},
+			'complianceCurrent': {'type':'float', 'units':'A', 'default': 1e-6},
+			'complianceVoltage': {'type':'float', 'units':'V', 'default': 10},,
+			'deviceMeasurementSpeed': {'type':'float', 'units':'Hz', 'default': 60},  # what are the units for this?
+		},
+		'Delay':{
+			'dependencies':[],
+			'message':{'type':'string', 'default': ""},
+			'delayTime':{'type':'int', 'units':'s', 'default': 240}
+		}
+
+	},
+	'Results':{
+		'default':{}
+	},
+	'Computed':{
+		'default':{}
+	},
+	'SensorData':{
+		'default':{}
+	},
+	'Identifiers':{
+		'user':{'type':'string'},
+		'project':{'type':'string'},
+		'wafer':{'type':'string'},
+		'chip':{'type':'string'},
+		'device':{'type':'string'},
+		'step':{'type':'int'},
+		'default':{}
+	},
+	'MeasurementSystem':{
+		'system': {'type':'choice','choices':['B2912A','PCB2v14'], 'default':['single', 'standalone', 'double'][1]},
+		'NPLC': {'type':'float', 'units':'V'},
+		'deviceRange': {'type':'array', 'default':[]}
+	},
+	'dataFolder': {'type':'string', 'default':'../../AutexysData'},
+	'ParametersFormatVersion': {'default': 4}	
+}
+
+
+
 default_parameters_description = {
 	'runConfigs': {
 		'GateSweep':{
