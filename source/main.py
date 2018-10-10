5a1609a6eecd676e24289f21fd71a2a0e12dcd24
stevennoyce <steven.noyce@gmail.com>
2018-10-10 09:45:22 -0400

    Starting to create a manager that can launch separate processes

--- source/main.py
+++ source/main.py
@@ -35,9 +35,11 @@ def send_notification_via_pushbullet(title, body):
 
 
 # === Main ===
-def main():
+def main(argChoice = None):
 	if len(sys.argv) > 1:
 		choice = sys.argv[1]
+	elif argChoice is not None:
+		choice = argChoice
 	else:
 		# Get user's action selection
 		choice = selectFromDictionary('Actions: ', runTypes, 'Choose an action (0,1,2,...): ')
