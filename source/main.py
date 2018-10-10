00a8516155ca78b2533e74b518c742adf0380d68
Jay Doherty <jayldoherty@gmail.com>
2018-10-10 12:11:02 -0400

    Updated main.py to get rid of old user interface prompts that are no longer useful

--- source/main.py
+++ source/main.py
@@ -3,74 +3,39 @@ import os
 import sys
 import platform
 import time
+import requests
+import json
 
 import launcher
 from utilities import DataLoggerUtility as dlu
 
-
-if __name__ == '__main__':
-	os.chdir(sys.path[0])
-
+if(__name__ == '__main__'):
 	pathParents = os.getcwd().split('/')
 	if 'AutexysHost' in pathParents:
 		os.chdir(os.path.join(os.path.abspath(os.sep), *pathParents[0:pathParents.index('AutexysHost')+1], 'source'))
 
 
-default_additional_parameters = {}
-
-
-import requests
-import json
- 
-def send_notification_via_pushbullet(title, body):
-	url = 'https://api.pushbullet.com/v2/pushes'
-	access_token = 'o.jc84248QDFZCW8QJWu9DXpzaLbdwhoD7'
-	data_send = {"type": "note", "title": title, "body": body}
-	headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
- 	
-	resp = requests.post(url, data=json.dumps(data_send), headers=headers)
-	
-	if resp.status_code != 200:
-		print('Pushbullet Notification Failed')
-	else:
-		print('Pushbullet Notification Succeeded')
-
 
 # === Main ===
 def main(schedulePath = None):
-	if len(sys.argv) > 1:
+	if((__name__ == '__main__') and (len(sys.argv) > 1)):
 		choice = sys.argv[1]
-	elif schedulePath is not None:
+	elif(schedulePath is not None):
 		choice = schedulePath
 	else:
-		# Get user's action selection
-		choice = selectFromDictionary('Actions: ', runTypes, 'Choose an action (0,1,2,...): ')
+		choice = input('Choose a schedule file to run: ')
 	
-	if(choice.isdigit()):
-		choice = int(choice)
-		if(choice == 0):
-			return
-		
-		additional_parameters = default_additional_parameters.copy()
-		additional_parameters['runType'] = runTypes[choice]
-
-		# Allow user to confirm the parameters before continuing
-		confirmation = str(selectFromDictionary('Parameters: ', additional_parameters, 'Do you want to run with defaults, plus these additional parameters? (y/n): '))
-		if(confirmation != 'y'):
-			return
-		
-		launcher.run(additional_parameters)
+	# File must end in '.json'
+	file = choice if(choice[-5:] == '.json') else (choice + '.json')
 	
-	else:
-		# File must end in '.json'
-		file = choice if(choice[-5:] == '.json') else (choice + '.json')
-		
-		run_file(file)
+	run_file(file)
 	
 	send_notification_via_pushbullet(
 		'Completed Main at {}'.format(time.strftime('%I:%M %p on %a')), 
 		'Script has finished choice of: {}'.format(choice)
 	)
+
+
 
 def run_file(schedule_file_path):
 	schedule_index = 0
@@ -87,28 +52,27 @@ def run_file(schedule_file_path):
 		launcher.run(additional_parameters)
 
 		schedule_index += 1
+	
+	print('Closing schedule file: ' + schedule_file_path)
+		
+		
+
+def send_notification_via_pushbullet(title, body):
+	url = 'https://api.pushbullet.com/v2/pushes'
+	access_token = 'o.jc84248QDFZCW8QJWu9DXpzaLbdwhoD7'
+	data_send = {"type": "note", "title": title, "body": body}
+	headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
+ 	
+	resp = requests.post(url, data=json.dumps(data_send), headers=headers)
+	
+	if resp.status_code != 200:
+		print('Pushbullet Notification Failed')
+	else:
+		print('Pushbullet Notification Succeeded')
+
 
 
 # === User Interface ===
-runTypes = {
-	'text':'schedule file',
-	0:'Quit',
-	1:'GateSweep',
-	2:'BurnOut',
-	3:'AutoBurnOut',
-	4:'StaticBias',
-	5:'AutoGateSweep',
-	6:'AutoStaticBias',
-	7:'DeviceHistory',
-	8:'ChipHistory'
-}
-
-# Present a dictionary of options to the user and get their choice.
-def selectFromDictionary(titleString, dictionary, promptString):
-	print(titleString)
-	print_dict(dictionary, 0)
-	return input(promptString)
-
 # Print a nicely formatted dictionary.
 def print_dict(dictionary, numtabs):
 	keys = list(dictionary.keys())
