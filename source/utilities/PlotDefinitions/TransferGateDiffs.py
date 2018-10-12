b715ead40ef2c31e50bf47dcc190f667b796eb5e
stevennoyce <steven.noyce@gmail.com>
2018-10-12 16:38:26 -0400

    Added a new plot

---
+++ source/utilities/PlotDefinitions/TransferGateDiffs.py
@@ -0,0 +1,80 @@
+from utilities.MatplotlibUtility import *
+
+
+
+plotDescription = {
+	'plotCategory': 'device',
+	'dataFileDependencies': ['GateSweep.json', 'ThisPlotIsStillInProgressDontUseItYet'],
+	'plotDefaults': {
+		'figsize':(2.8,3.2),
+		'includeOrigin':True,
+		'colorMap':'viridis',
+		'colorDefault': plt.rcParams['axes.prop_cycle'].by_key()['color'][1],
+		'xlabel':'$V_{{GS}}^{{Sweep}}$ [V]',
+		'ylabel':'$I_{{D}}$ [$\\mu$A]',
+		'neg_label':'$-I_{{D}}$ [$\\mu$A]',
+		'ii_label':'$I_{{D}}$, $I_{{G}}$ [$\\mu$A]',
+		'neg_ii_label':'$-I_{{D}}$, $I_{{G}}$ [$\\mu$A]',
+		'leg_vds_label':'$V_{{DS}}^{{Sweep}}$\n  = {:}V',
+		'leg_vds_range_label':'$V_{{DS}}^{{min}} = $ {:}V\n'+'$V_{{DS}}^{{max}} = $ {:}V',
+	},
+}
+
+def plot(deviceHistory, identifiers, mode_parameters=None):
+	# Init Figure
+	fig, ax = initFigure(1, 1, plotDescription['plotDefaults']['figsize'], figsizeOverride=mode_parameters['figureSizeOverride'])
+	if(not mode_parameters['publication_mode']):
+		ax.set_title(getTestLabel(deviceHistory, identifiers))
+	
+	# Build Color Map and Color Bar
+	totalTime = timeWithUnits(deviceHistory[-1]['Results']['timestamps'][0][0] - deviceHistory[0]['Results']['timestamps'][-1][-1])
+	holdTime = '[$t_{{Hold}}$ = {}]'.format(timeWithUnits(deviceHistory[1]['Results']['timestamps'][-1][-1] - deviceHistory[0]['Results']['timestamps'][0][0])) if(len(deviceHistory) >= 2) else ('[$t_{{Hold}}$ = 0]')
+	colors = setupColors(fig, len(deviceHistory), colorOverride=mode_parameters['colorsOverride'], colorDefault=plotDescription['plotDefaults']['colorDefault'], colorMapName=plotDescription['plotDefaults']['colorMap'], colorMapStart=0.7, colorMapEnd=0, enableColorBar=mode_parameters['enableColorBar'], colorBarTicks=[0,0.6,1], colorBarTickLabels=[totalTime, holdTime, '$t_0$'], colorBarAxisLabel='')		
+	
+	# If first segment of device history is mostly negative current, flip data
+	if((len(deviceHistory) > 0) and (np.percentile(deviceHistory[0]['Results']['id_data'], 75) < 0)):
+		deviceHistory = scaledData(deviceHistory, 'Results', 'id_data', -1)
+		plotDescription['plotDefaults']['ylabel'] = plotDescription['plotDefaults']['neg_label']
+	
+	# Plot
+	for i in range(len(deviceHistory)):
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][0], deviceHistory[i]['Results']['id_data'][0])
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][1], deviceHistory[i]['Results']['id_data'][1])
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][0], deviceHistory[i]['Results']['id_data'][0] - deviceHistory[i]['Results']['ig_data'][0])
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][1], deviceHistory[i]['Results']['id_data'][1] - deviceHistory[i]['Results']['ig_data'][0])
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][0], deviceHistory[i]['Results']['id_data'][0] + deviceHistory[i]['Results']['ig_data'][0])
+		ax.plot(deviceHistory[i]['Results']['vgs_data'][1], deviceHistory[i]['Results']['id_data'][1] + deviceHistory[i]['Results']['ig_data'][0])
+		
+		# line1 = plotTransferCurve(ax, deviceHistory[i], colors[i], direction=mode_parameters['sweepDirection'], scaleCurrentBy=1e6, lineStyle=None, errorBars=mode_parameters['enableErrorBars'])
+		# line2 = plotTransferCurve(ax, deviceHistory[i], colors[i], direction=mode_parameters['sweepDirection'], scaleCurrentBy=1e6, lineStyle=None, errorBars=mode_parameters['enableErrorBars'])
+		# line3 = plotTransferCurve(ax, deviceHistory[i], colors[i], direction=mode_parameters['sweepDirection'], scaleCurrentBy=1e6, lineStyle=None, errorBars=mode_parameters['enableErrorBars'])
+		# if(len(deviceHistory) == len(mode_parameters['legendLabels'])):
+		# 	setLabel(line, mode_parameters['legendLabels'][i])
+
+	axisLabels(ax, x_label=plotDescription['plotDefaults']['xlabel'], y_label=plotDescription['plotDefaults']['ylabel'])
+
+	# Add gate current to axis
+	if(mode_parameters['includeGateCurrent']):
+		if(len(deviceHistory) == 1):
+			gate_colors = [plt.rcParams['axes.prop_cycle'].by_key()['color'][2]]
+			gate_linestyle = None
+		else:	
+			gate_colors = colors
+			gate_linestyle = '--'
+		for i in range(len(deviceHistory)):
+			plotGateCurrent(ax, deviceHistory[i], gate_colors[i], direction=mode_parameters['sweepDirection'], scaleCurrentBy=1e6, lineStyle=gate_linestyle, errorBars=mode_parameters['enableErrorBars'])
+		if(plotDescription['plotDefaults']['ylabel'] == plotDescription['plotDefaults']['neg_label']):
+			plotDescription['plotDefaults']['ylabel'] = plotDescription['plotDefaults']['neg_ii_label']
+		else:
+			plotDescription['plotDefaults']['ylabel'] = plotDescription['plotDefaults']['ii_label']
+		axisLabels(ax, x_label=plotDescription['plotDefaults']['xlabel'], y_label=plotDescription['plotDefaults']['ylabel'])
+
+	# Adjust Y-lim (if desired)
+	includeOriginOnYaxis(ax, include=plotDescription['plotDefaults']['includeOrigin'])
+
+	# Add Legend and save figure	
+	addLegend(ax, loc=mode_parameters['legendLoc'], title=getLegendTitle(deviceHistory, identifiers, plotDescription['plotDefaults'], 'runConfigs', 'GateSweep', mode_parameters, includeVdsSweep=True))
+	adjustAndSaveFigure(fig, 'FullTransferCurves', mode_parameters)
+
+	return (fig, ax)
+
