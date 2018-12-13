56d88526d421c46d40660d9178335e3e4695b255
Jay Doherty <jayldoherty@gmail.com>
2018-12-13 17:46:30 -0500

    Separated AFM image plots from the scatter plot

--- source/utilities/PlotDefinitions/AFMdeviationsImage.py
+++ source/utilities/PlotDefinitions/AFMdeviationsImage.py
@@ -17,51 +17,31 @@ def plot(deviceHistory, identifiers, mode_parameters=None):
 	if(not mode_parameters['publication_mode']):
 		ax.set_title(getTestLabel(deviceHistory, identifiers))
 	
-	# Build Color Map and Color Bar	
-	colors = colorsFromMap(plotDescription['plotDefaults']['colorMap'], 0, 0.87, len(deviceHistory))['colors']
-	
+	# Get X/Y limits
 	Vxs = []
 	Vys = []
-	currents = []
-	
-	# Plot
 	for i in range(len(deviceHistory)):
-		current = np.array(deviceHistory[i]['Results']['id_data'])
-		currentLinearFit = np.polyval(np.polyfit(range(len(current)), current, 1), range(len(current)))
-		currentLinearized = current - currentLinearFit
-		currentLinearized = currentLinearized - np.median(currentLinearized)
-		
-		# currentLinearized[np.where(np.array(deviceHistory[i]['Results']['smu2_v1_data']) - max(deviceHistory[i]['Results']['smu2_v1_data']) < -2.2*0.157)[0]] = 0
-		
 		Vxs.extend(deviceHistory[i]['Results']['smu2_v2_data'])
 		Vys.extend(deviceHistory[i]['Results']['smu2_v1_data'])
-		currents.extend(currentLinearized)
-	
 	Xs = -np.array(Vxs)/0.157
 	Ys = np.array(Vys)/0.138
-	
 	Xs = Xs - np.min(Xs)
 	Ys = Ys - np.min(Ys)
 	
-	# currents[np.where(Xs < 0.5)[0]] = 0
-	
-	c, a, b = zip(*sorted(zip(np.array(currents)*1e9, Xs, Ys), reverse=False))
-	line = ax.scatter(a, b, c=c, cmap=plotDescription['plotDefaults']['colorMap'], alpha=0.6, marker='o')
-	# line = ax.scatter(Xs, Ys, c=np.array(currents)*1e9, cmap=plotDescription['plotDefaults']['colorMap'], alpha=0.6)
-	cbar = fig.colorbar(line, pad=0.015, aspect=50)
-	cbar.set_label('Drain Current [nA]', rotation=270, labelpad=11)
-	cbar.solids.set(alpha=1)
-	
-	# if(len(deviceHistory) == len(mode_parameters['legendLabels'])):
-		# setLabel(line, mode_parameters['legendLabels'][i])
-	
+	# Axis Labels
 	ax.set_ylabel('Y Position ($\\mu$m)')
 	ax.set_xlabel('X Position ($\\mu$m)')
 	
+	# Get data
+	Vx_vals = extractTraces(deviceHistory)['Vx'][0]
+	Vy_vals = extractTraces(deviceHistory)['Vy'][0]
+	Id_vals = extractTraces(deviceHistory)['Id'][0]
+	
+	# Plot
+	ax.imshow(getRasteredMatrix(Vx_vals, Vy_vals, Id_vals), extent=(min(Xs), max(Xs), min(Ys), max(Ys)), interpolation=None)
+	
 	# Add Legend and save figure
 	adjustAndSaveFigure(fig, 'AFMdeviationsVsXY', mode_parameters)
-	
-	
 	
 	return (fig, ax)
 	
