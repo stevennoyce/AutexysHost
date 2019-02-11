f47a14edc25fe1e4e16ea49d3b9145f6ba05d484
Matthew Barbano <matthew.barbano@duke.edu>
2019-02-11 00:39:34 -0500

    Fixing git

--- source/procedures/Device_History.py
+++ source/procedures/Device_History.py
@@ -4,7 +4,7 @@
 if(__name__ == '__main__'):
 	import sys
 	sys.path.append(sys.path[0] + '/..')
-	
+
 	import os
 	pathParents = os.getcwd().split('/')
 	if('AutexysHost' in pathParents):
@@ -35,7 +35,7 @@ default_dh_parameters = {
 # === External Interface ===
 def makePlots(userID, projectID, waferID, chipID, deviceID, minExperiment=0, maxExperiment=float('inf'), specificPlot='', figureSize=None, sweepDirection='both', dataFolder=None, saveFolder=None, plotSaveName='', saveFigures=False, showFigures=True, minRelativeIndex=0, maxRelativeIndex=float('inf'), plot_mode_parameters=None):
 	"""Make plots for the device found in the userID/projectID/waferID/chipID/deviceID folder.
-	
+
 	minExperiment and maxExperiment specify a range of experiments to include in the plot(s).
 	specificPlot can be specified to only make one specific plot found in the plotDefintions folder, or by default all available plots are made.
 	figureSize can be specified as (width,height) to set the size of the plot.
@@ -44,15 +44,15 @@ def makePlots(userID, projectID, waferID, chipID, deviceID, minExperiment=0, max
 	saveFigures and showFigures are booleans that specify if a plot should be shown with the matplotlib pyplot interface or saved as a .png
 	sweepDirection is a commonly used plot decorator (can be 'both', 'forward', or 'reverse').
 	minRelativeIndex and maxRelativeIndex can be used to limit the number of data entries shown if all entries have the same experimentNumber
-	
+
 	plot_mode_parameters is a catch-all dictionary for parameters that affect the style of plots (but not the data shown!). See the DataPlotterUtility
 	for more information about available plot_mode_parameters."""
-	
-	parameters = {}	
+
+	parameters = {}
 	mode_parameters = {}
 	if(plot_mode_parameters is not None):
 		mode_parameters.update(plot_mode_parameters)
-	
+
 	# Data loading parameters
 	parameters['Identifiers'] = {}
 	parameters['Identifiers']['user'] = userID
@@ -66,11 +66,11 @@ def makePlots(userID, projectID, waferID, chipID, deviceID, minExperiment=0, max
 	parameters['maxJSONExperimentNumber'] = maxExperiment
 	parameters['minJSONRelativeIndex'] = minRelativeIndex
 	parameters['maxJSONRelativeIndex'] = maxRelativeIndex
-	
+
 	# Plot selection parameters
 	parameters['showFigures'] = showFigures
 	parameters['specificPlotToCreate'] = specificPlot
-	
+
 	# Plot decoration parameters
 	if(saveFolder is not None):
 		mode_parameters['plotSaveFolder'] = saveFolder
@@ -79,7 +79,7 @@ def makePlots(userID, projectID, waferID, chipID, deviceID, minExperiment=0, max
 	mode_parameters['plotSaveName'] = plotSaveName
 	mode_parameters['figureSizeOverride'] = figureSize
 	mode_parameters['sweepDirection'] = sweepDirection
-	
+
 	return run(parameters, plot_mode_parameters=mode_parameters)
 
 
@@ -87,7 +87,7 @@ def makePlots(userID, projectID, waferID, chipID, deviceID, minExperiment=0, max
 # === Main ===
 def run(additional_parameters, plot_mode_parameters=None):
 	"""Legacy 'run' function from when DeviceHistory was treated more like a typical procedure with parameters."""
-	
+
 	parameters = default_dh_parameters.copy()
 	parameters.update(additional_parameters)
 
@@ -100,7 +100,7 @@ def run(additional_parameters, plot_mode_parameters=None):
 
 	# Print information about the device and experiment being plotted
 	print('  ' + parameters['Identifiers']['wafer'] + parameters['Identifiers']['chip'] + ':' + parameters['Identifiers']['device'])
-	'' if((p['minJSONExperimentNumber'] == 0) and (p['maxJSONExperimentNumber'] == float('inf'))) else (print('  Experiment #{:}'.format(p['maxJSONExperimentNumber'])) if(p['minJSONExperimentNumber'] == p['maxJSONExperimentNumber']) else (print('  Experiments #{:} to #{:}'.format(p['minJSONExperimentNumber'],p['maxJSONExperimentNumber'])))) 
+	'' if((p['minJSONExperimentNumber'] == 0) and (p['maxJSONExperimentNumber'] == float('inf'))) else (print('  Experiment #{:}'.format(p['maxJSONExperimentNumber'])) if(p['minJSONExperimentNumber'] == p['maxJSONExperimentNumber']) else (print('  Experiments #{:} to #{:}'.format(p['minJSONExperimentNumber'],p['maxJSONExperimentNumber']))))
 	'' if((p['minJSONRelativeIndex'] == 0) and (p['maxJSONRelativeIndex'] == float('inf'))) else (print('  Rel. Index #{:}'.format(p['maxJSONRelativeIndex'])) if(p['minJSONRelativeIndex'] == p['maxJSONRelativeIndex']) else (print('  Rel. Indices #{:} to #{:}'.format(p['minJSONRelativeIndex'],p['maxJSONRelativeIndex']))))
 	'' if((p['minJSONIndex'] == 0) and (p['maxJSONIndex'] == float('inf'))) else (print('  Abs. Index #{:}'.format(p['maxJSONIndex'])) if(p['minJSONIndex'] == p['maxJSONIndex']) else (print('  Abs. Indices #{:} to #{:}'.format(p['minJSONIndex'],p['maxJSONIndex']))))
 
@@ -111,7 +111,7 @@ def run(additional_parameters, plot_mode_parameters=None):
 			mode_parameters['generalInfo'] = wafer_info
 		except:
 			print('Error: no information in wafer.json for this device.')
-	
+
 	# Determine which plots are being requested and make them all
 	plotsToCreate = [p['specificPlotToCreate']] if(p['specificPlotToCreate'] != '') else plotsForExperiments(parameters, minExperiment=p['minJSONExperimentNumber'], maxExperiment=p['maxJSONExperimentNumber'])
 	for plotType in plotsToCreate:
@@ -128,8 +128,8 @@ def run(additional_parameters, plot_mode_parameters=None):
 			print(e)
 			import traceback
 			traceback.print_exc()
-	
-	# Show figures if desired		
+
+	# Show figures if desired
 	if(p['showFigures']):
 		dpu.show()
 
@@ -139,14 +139,14 @@ def run(additional_parameters, plot_mode_parameters=None):
 
 def plotsForExperiments(parameters, minExperiment=0, maxExperiment=float('inf')):
 	"""Given the typical parameters used to run experiments, return a list of plots that could be made from the data that has been already collected."""
-	
+
 	return dpu.getPlotTypesFromDependencies(dlu.getDataFileNamesForDeviceExperiments(dlu.getDeviceDirectory(parameters), minExperiment=minExperiment, maxExperiment=maxExperiment), plotCategory='device')
 
 
 
 if __name__ == '__main__':
 	"""Feel free to add your own makePlots function calls to try this out!"""
-	
+
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 145, 145, 'FullStaticBiasHistory', (3.5,2.24), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S6 floating - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True, 'staticBiasSegmentDividers':True, 'enableGradient':True})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 118, 118, 'FullStaticBiasHistory', (3.5,2.24), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S5 Grounded Between - ', saveFigures=True, showFigures=False, minRelativeIndex=9, maxRelativeIndex=16, plot_mode_parameters={'publication_mode':True,'staticBiasSegmentDividers':True, 'enableGradient':True})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '27-28', 3, 4, 'FullTransferCurveHistory', (1.48 *2.24/1.74,1.74 *2.24/1.74), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S1 Comparison - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True, 'enableColorBar':False})
@@ -155,9 +155,9 @@ if __name__ == '__main__':
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 125, 125, 'FullStaticBiasHistory', (2.2 *3.25/2.2,2.24), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S9b Light - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True, 'enableGradient':True})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 127, 127, 'FullSubthresholdCurveHistory', (1.45 *2.24/1.55,1.55 *2.24/1.55), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S10a Light - ', saveFigures=True, showFigures=False, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 125, 125, 'FullSubthresholdCurveHistory', (1.45 *2.24/1.55,1.55 *2.24/1.55), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S10b Light - ', saveFigures=True, showFigures=False, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False})
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'SubthresholdCurve', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure S12a - ', saveFigures=True, showFigures=True, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False}) 
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'TransferCurve', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure S12b - ', saveFigures=True, showFigures=True, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False}) 
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'StaticBias', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure 3a - ', saveFigures=True, showFigures=True, plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False}) 
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'SubthresholdCurve', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure S12a - ', saveFigures=True, showFigures=True, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'TransferCurve', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure S12b - ', saveFigures=True, showFigures=True, sweepDirection='both', plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 104, 104, 'StaticBias', (1.45 *2.24/1.55,2.24), dataFolder='../../AutexysData', plotSaveName='Figure 3a - ', saveFigures=True, showFigures=True, plot_mode_parameters={'publication_mode':True, 'enableErrorBars':False})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 24, 24, 'FullSubthresholdCurveHistory', (1.45 *2.24/1.55,1.55 *2.24/1.55), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S11a - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True,'enableErrorBars':False})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 24, 24, 'FullTransferCurveHistory', (1.48 *2.24/1.74,1.74 *2.24/1.74), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S11b - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True,'enableErrorBars':False})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 155, 155, 'FullStaticBiasHistory', (3.5,2.24), dataFolder='../data', saveFolder='../CurrentPlots', plotSaveName='Figure S7 no grounding or floating - ', saveFigures=True, showFigures=False, plot_mode_parameters={'publication_mode':True,'staticBiasChangeDividers':False,'enableGradient':True})
@@ -175,20 +175,18 @@ if __name__ == '__main__':
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'X', '15-16', 80, 168, 'OnAndOffCurrentHistory', (2.2 *5.78/2.2,1.408 *3.5/2.2), dataFolder='../../../AutexysData', saveFolder='../../../AutexysPlots/', plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':True, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'lower left'})
 	#makePlots('stevenjay', 'BiasStress1', 'C127', 'P', '15-16', 0, 500, '', None, dataFolder='../../../AutexysData', saveFolder='../../../AutexysPlots/', plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
 	#makePlots('stevenjay', 'SolutionBias1', 'C127', 'V', '2-3', 0, 500, 'FullOutputCurveHistory', None, dataFolder='../../../AutexysData', saveFolder='../../../AutexysPlots/', plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'P', '1-2', 5, 7, 'SubthresholdCurve', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})	
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '15-16', 10, 18, 'OnAndOffCurrentHistory', None, dataFolder='../../AutexysData', saveFolder='../../../AutexysPlots/', plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})	
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'P', '1-2', 5, 7, 'SubthresholdCurve', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '15-16', 10, 18, 'OnAndOffCurrentHistory', None, dataFolder='../../AutexysData', saveFolder='../../../AutexysPlots/', plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
 	#makePlots('steven', 'SGM1', 'F1', 'E', 'E08N_10000', 51, 51, 'AFMdeviationsVsX', None, dataFolder='../data', showFigures=True)
-	#makePlots('jay', 'MoS2FET', 'JM3', 'B', '53-54', 9, 9, 'StaticBias', None, plotSaveName='Figure S17b full - ', saveFigures=True, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'colorsOverride':['#56638A','#56638A','#56638A']})	
-	#makePlots('matthew', 'TFTBiasStress1', 'C144', 'T', '13-14', 1, 1, 'GateCurrent', None, plotSaveName='Matthew4', dataFolder='../../AutexysData', showFigures=True, saveFigures=True)
-	#makePlots('jay', 'MoS2FET', 'JM3', 'B', '53-54', 277, 277, 'TransferCurve', None, plotSaveName='Figure S17b full - ', saveFigures=True, showFigures=True, minRelativeIndex=1, maxRelativeIndex=16, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})	
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '15-16', 6, 10, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})	
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 14, 17, 'StaticBias', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})	
-	makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 14, 15, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})	
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 15, 15, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})	
-	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 16, 16, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})	
+	#makePlots('jay', 'MoS2FET', 'JM3', 'B', '53-54', 9, 9, 'StaticBias', None, plotSaveName='Figure S17b full - ', saveFigures=True, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'colorsOverride':['#56638A','#56638A','#56638A']})
+	makePlots('matthew', 'TFTBiasStress1', 'C144', 'T', '13-14', 1, 1, 'GateCurrent', None, plotSaveName='Matthew4', dataFolder='../../AutexysData', showFigures=True, saveFigures=True)
+	#makePlots('jay', 'MoS2FET', 'JM3', 'B', '53-54', 277, 277, 'TransferCurve', None, plotSaveName='Figure S17b full - ', saveFigures=True, showFigures=True, minRelativeIndex=1, maxRelativeIndex=16, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '15-16', 6, 10, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 14, 17, 'StaticBias', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best'})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 14, 15, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 15, 15, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})
+	#makePlots('stevenjay', 'BiasStress1', 'C127', 'E', '14-15', 16, 16, 'OnOffCurrent', None, plotSaveName='Figure S17b full - ', saveFigures=False, showFigures=True, plot_mode_parameters={'publication_mode':False, 'staticBiasChangeDividers':False, 'enableGradient':False, 'legendLoc':'best', 'includeDualAxis':False, 'includeOffCurrent':False, 'enableLegend':False})
+
+	#makePlots('matthew', 'breadboard_FET', '1', '1', '1', 1, 1, 'SignalToNoiseRatio', None, dataFolder='../../AutexysData', showFigures=True)
+
 	pass
-
-
-
-
-
