b57986697567a8fcddc9367bd068292735fdee2c
Jay Doherty <jayldoherty@gmail.com>
2019-06-21 14:53:56 -0400

    Refactored AutoStaticBias to expect array inputs instead of incrementers

--- source/procedures/Auto_Static_Bias.py
+++ source/procedures/Auto_Static_Bias.py
@@ -24,33 +24,20 @@ def runAutoStaticBias(parameters, smu_instance, arduino_instance, gateSweepParam
 	sb_parameters = staticBiasParameters['runConfigs']['StaticBias']
 	asb_parameters = parameters['runConfigs']['AutoStaticBias']
 
-	numberOfStaticBiases = asb_parameters['numberOfStaticBiases']
+	importantListLengths = [len(asb_parameters['biasTimeList']), len(asb_parameters['gateVoltageSetPointList']), len(asb_parameters['drainVoltageSetPointList']), len(asb_parameters['gateVoltageWhenDoneList']), len(asb_parameters['drainVoltageWhenDoneList'])]
+	numberOfStaticBiases = max(asb_parameters['numberOfStaticBiases'], max(importantListLengths))
 	
 	# Build arrays of all parameters that could change over the course of any given experiement
-	biasTimeList 				= asb_parameters['biasTimeList'] if(len(asb_parameters['biasTimeList']) == numberOfStaticBiases) else ([sb_parameters['totalBiasTime']]*numberOfStaticBiases)
-	gateVoltageSetPointList 	= [sb_parameters['gateVoltageSetPoint']]*numberOfStaticBiases
-	drainVoltageSetPointList 	= [sb_parameters['drainVoltageSetPoint']]*numberOfStaticBiases
-	gateVoltageWhenDoneList 	= [sb_parameters['gateVoltageWhenDone']]*numberOfStaticBiases
-	drainVoltageWhenDoneList 	= [sb_parameters['drainVoltageWhenDone']]*numberOfStaticBiases
-	delayWhenDoneList 			= [sb_parameters['delayWhenDone']]*numberOfStaticBiases
+	biasTimeList 				= asb_parameters['biasTimeList'] 				+ [sb_parameters['totalBiasTime']]       *(numberOfStaticBiases-len(asb_parameters['biasTimeList']))
+	gateVoltageSetPointList 	= asb_parameters['gateVoltageSetPointList'] 	+ [sb_parameters['gateVoltageSetPoint']] *(numberOfStaticBiases-len(asb_parameters['gateVoltageSetPointList']))
+	drainVoltageSetPointList 	= asb_parameters['drainVoltageSetPointList'] 	+ [sb_parameters['drainVoltageSetPoint']]*(numberOfStaticBiases-len(asb_parameters['drainVoltageSetPointList']))
+	gateVoltageWhenDoneList 	= asb_parameters['gateVoltageWhenDoneList'] 	+ [sb_parameters['gateVoltageWhenDone']] *(numberOfStaticBiases-len(asb_parameters['gateVoltageWhenDoneList']))
+	drainVoltageWhenDoneList 	= asb_parameters['drainVoltageWhenDoneList'] 	+ [sb_parameters['drainVoltageWhenDone']]*(numberOfStaticBiases-len(asb_parameters['drainVoltageWhenDoneList']))
+	delayWhenDoneList 			= asb_parameters['delayWhenDoneList'] 			+ [sb_parameters['delayWhenDone']]	     *(numberOfStaticBiases-len(asb_parameters['delayWhenDoneList']))
 	delayBeforeMeasurementsList = [sb_parameters['delayBeforeMeasurementsBegin']]*numberOfStaticBiases
 
-	# Modify parameter arrays so that they increment there values as desired
-	currentIncrementNumber = 1
-	for i in range(numberOfStaticBiases):
-		if(i >= asb_parameters['numberOfBiasesBetweenIncrements']*currentIncrementNumber):
-			currentIncrementNumber += 1
-		gateVoltageSetPointList[i] 	+= asb_parameters['incrementStaticGateVoltage']*(currentIncrementNumber-1)
-		drainVoltageSetPointList[i] += asb_parameters['incrementStaticDrainVoltage']*(currentIncrementNumber-1)
-		gateVoltageWhenDoneList[i] 	+= asb_parameters['incrementGateVoltageWhenDone']*(currentIncrementNumber-1)
-		drainVoltageWhenDoneList[i] += asb_parameters['incrementDrainVoltageWhenDone']*(currentIncrementNumber-1)
-		delayWhenDoneList[i] 		+= asb_parameters['incrementDelayBeforeReapplyingVoltage']*(currentIncrementNumber-1)	
 	delayBeforeMeasurementsList[0] = asb_parameters['firstDelayBeforeMeasurementsBegin']
-
-	# Randomize the time spent grounding the terminals if desired
-	if(asb_parameters['shuffleDelaysBeforeReapplyingVoltage']):
-		random.shuffle(delayWhenDoneList)
-
+	
 
 
 	## === START ===
