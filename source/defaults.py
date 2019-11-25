31a359087ba2e75c315fba8ce6963e1835d40432
Jay Doherty <jayldoherty@gmail.com>
2019-11-25 16:42:05 -0500

    Added "combo" system configuration as an option in the UI

--- source/defaults.py
+++ source/defaults.py
@@ -238,7 +238,7 @@ default_parameters = {
 		
 	},
 	'MeasurementSystem':{
-		'systemType': {'type':'choice', 'choices':['single', 'standalone', 'bluetooth', 'inverter', 'double', 'emulator'], 'default':['single', 'standalone', 'bluetooth', 'inverter', 'double', 'emulator'][0], 'title':'System Type', 'description':''},
+		'systemType': {'type':'choice', 'choices':['single', 'standalone', 'bluetooth', 'combo', 'inverter', 'double', 'emulator'], 'default':['single', 'standalone', 'bluetooth', 'inverter', 'double', 'emulator'][0], 'title':'System Type', 'description':''},
 		'systems': {},
 		'deviceRange': {'type':'array', 'choices':["1-2", "2-3", "3-4", "5-6", "6-7", "7-8", "9-10", "10-11", "11-12", "13-14", "14-15", "15-16", "19-20", "21-22", "27-28", "29-30", "30-31", "31-32", "33-34", "34-35", "35-36", "37-38", "38-39", "39-40", "41-42", "42-43", "43-44", "45-46", "46-47", "47-48", "51-52", "53-54", "59-60", "61-62", "62-63", "63-64"], 'default':[], 'title':'Device Range', 'description':''} 
 	},
